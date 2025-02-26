c5f112e8ee610373023ecf0ababb1f0b18932048
Ahmed Farouk <ahmedmohamedfarouk212103048@gmail.com>
2025-02-26 09:22:02 +0200

    newstbooks cubit was done

--- lib/Features/Home/busines_logic/cubits/newsbookCubit/cubit/newstbooks_cubit.dart
+++ lib/Features/Home/busines_logic/cubits/newsbookCubit/cubit/newstbooks_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/Home/data/models/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repository/home_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'newstbooks_state.dart';
 
 class NewstbooksCubit extends Cubit<NewstbooksState> {
-  NewstbooksCubit() : super(NewstbooksInitial());
+  NewstbooksCubit(this.homeRepository) : super(NewstbooksInitial());
+  final HomeRepository homeRepository;
+  Future getNewBooks(String filter, String query, String orderBy, int maxResults) async {
+    emit(NewstbooksLoading());
+    try {
+      final newstbooks = await homeRepository.getNewBooks(filter, query, orderBy, maxResults);
+      newstbooks.fold(
+        (failure) {
+          emit(NewstbooksFailure(failure.toString()));
+        },
+        (newstbooks) {
+          emit(NewstbooksSuccess(newstbooks));
+        },
+      );
+    } catch (e) {
+      emit(NewstbooksFailure(e.toString()));
+    }
+  }
 }
